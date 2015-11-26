2c99e34911eb5cb837f55b34544d72238449c477
George Biryukov <g.birjukov@corp.mail.ru>
2015-11-26 18:45:16 +0300

    add global version

---
+++ demo/vanillajs/js/lib/loadic.global.js
@@ -0,0 +1,212 @@
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+    var INTERVAL_TIME = 500;
+    var DEFAULTS = {
+        common: {
+            type: Array,
+            value: []
+        },
+        dpiDependent: {
+            type: Array,
+            value: []
+        },
+        highDpiSuffix: {
+            type: String,
+            value: '@2x'
+        },
+        highDpiCondition: {
+            type: Function,
+            value: function value() {
+                return window.devicePixelRatio > 1;
+            }
+        },
+        timeout: {
+            type: Number,
+            value: false
+        }
+    };
+
+    var Loader = (function () {
+        function Loader(config) {
+            _classCallCheck(this, Loader);
+
+            // config sanitization
+            for (var key in DEFAULTS) {
+                if (!config[key] instanceof DEFAULTS[key].type) {
+                    config[key] = DEFAULTS[key].value;
+                }
+            }
+
+            // update filenames for high dpi resources
+            if (config.highDpiCondition()) {
+                config.dpiDependent = config.dpiDependent.map(function (item) {
+                    return item = item.replace(/(\..+)$/, config.highDpiSuffix + '$1');
+                });
+            }
+
+            this.progress = 0;
+            this.loadedCount = 0;
+            this.resources = this.getManifest(config);
+            this.resourcesTotal = this.resources.length;
+
+            if (config.timeout) {
+                this.scheduleComplete(config.timeout);
+            }
+
+            for (var i in this.resources) {
+                this.preload(this.resources[i]);
+            }
+
+            this.simulateProgress();
+
+            return this;
+        }
+
+        _createClass(Loader, [{
+            key: 'preload',
+            value: function preload(resource) {
+                var _this = this;
+
+                var isStyle = /.css$/.test(resource.src),
+                    srcName = 'data',
+                    element = document.createElement('object');
+
+                if (isStyle) {
+                    element = document.createElement('link');
+                    element.rel = 'stylesheet';
+                    srcName = 'href';
+                } else {
+                    element.width = 0;
+                    element.height = 0;
+                }
+
+                element.onload = function () {
+                    resource.loaded = true;
+                    _this.loaded(resource);
+                };
+
+                element.onerror = function () {
+                    _this.loaded(resource);
+                };
+
+                element[srcName] = resource.src;
+                document.body.appendChild(element);
+            }
+        }, {
+            key: 'loaded',
+            value: function loaded(resource) {
+                this.loadedCount++;
+                this.updateProgress();
+
+                if (resource.required && this.loadingTimedOut) {
+                    this.scheduleComplete(0);
+                }
+
+                if (this.loadedCount === this.resourcesTotal) {
+                    this.complete();
+                }
+            }
+        }, {
+            key: 'complete',
+            value: function complete() {
+                clearInterval(this.intervalId);
+                clearInterval(this.timeoutId);
+
+                if ('onComplete' in this) {
+                    this.onComplete();
+                }
+            }
+        }, {
+            key: 'updateProgress',
+            value: function updateProgress(forcePercent) {
+                var realProgress = parseInt(this.loadedCount / this.resourcesTotal * 100);
+
+                if (this.progress < realProgress) {
+                    this.progress = realProgress;
+                }
+
+                if (this.progress < forcePercent) {
+                    this.progress = forcePercent;
+                }
+
+                if ('onProgress' in this) {
+                    this.onProgress(this.progress);
+                }
+            }
+        }, {
+            key: 'scheduleComplete',
+            value: function scheduleComplete(timeout) {
+                var _this2 = this;
+
+                this.timeoutId = setTimeout(function () {
+                    var isAllRequiredLoaded = _this2.resources.some(function (resource) {
+                        return resource.required && resource.loaded;
+                    });
+
+                    if (isAllRequiredLoaded) {
+                        _this2.complete();
+                    } else {
+                        _this2.loadingTimedOut = true;
+                    }
+                }, timeout);
+            }
+        }, {
+            key: 'simulateProgress',
+            value: function simulateProgress() {
+                var _this3 = this;
+
+                var iteration = 0;
+
+                this.intervalId = setInterval(function () {
+                    iteration++;
+
+                    var progressInc = 5 / Math.ceil(iteration / 10) * (1 - _this3.progress / 100);
+
+                    if (_this3.progress + progressInc < 95) {
+                        _this3.updateProgress(_this3.progress + progressInc);
+                    } else {
+                        clearInterval(_this3.intervalId);
+                    }
+                }, INTERVAL_TIME);
+            }
+        }, {
+            key: 'getManifest',
+            value: function getManifest(config) {
+                var urls = config.common.concat(config.dpiDependent);
+
+                var resources = urls.map(function (url) {
+                    var resource = undefined;
+
+                    if (typeof url === 'string') {
+                        if (url.lastIndexOf('!') === url.length - 1) {
+                            resource = {
+                                src: url.substr(0, url.length - 1),
+                                required: true
+                            };
+                        } else {
+                            resource = { src: url };
+                        }
+                    } else {
+                        resource = url;
+                    }
+
+                    return resource;
+                });
+
+                return resources.sort(function (resource) {
+                    return resource.required ? -1 : 1;
+                });
+            }
+        }]);
+
+        return Loader;
+    })();
+
+    this.window.loadic = Loader;
+    ;
+}).call(this);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,51 +5,59 @@ const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
 let ES2015BasePlugins = [
-    "babel-plugin-transform-es2015-template-literals",
-    "babel-plugin-transform-es2015-literals",
-    "babel-plugin-transform-es2015-function-name",
-    "babel-plugin-transform-es2015-arrow-functions",
-    "babel-plugin-transform-es2015-block-scoped-functions",
-    "babel-plugin-transform-es2015-classes",
-    "babel-plugin-transform-es2015-object-super",
-    "babel-plugin-transform-es2015-shorthand-properties",
-    "babel-plugin-transform-es2015-computed-properties",
-    "babel-plugin-transform-es2015-for-of",
-    "babel-plugin-transform-es2015-sticky-regex",
-    "babel-plugin-transform-es2015-unicode-regex",
-    "babel-plugin-check-es2015-constants",
-    "babel-plugin-transform-es2015-spread",
-    "babel-plugin-transform-es2015-parameters",
-    "babel-plugin-transform-es2015-destructuring",
-    "babel-plugin-transform-es2015-block-scoping",
-    "babel-plugin-transform-es2015-typeof-symbol",
-    ["babel-plugin-transform-regenerator", { "async": false, "asyncGenerators": false }]
-]
+    'babel-plugin-transform-es2015-template-literals',
+    'babel-plugin-transform-es2015-literals',
+    'babel-plugin-transform-es2015-function-name',
+    'babel-plugin-transform-es2015-arrow-functions',
+    'babel-plugin-transform-es2015-block-scoped-functions',
+    'babel-plugin-transform-es2015-classes',
+    'babel-plugin-transform-es2015-object-super',
+    'babel-plugin-transform-es2015-shorthand-properties',
+    'babel-plugin-transform-es2015-computed-properties',
+    'babel-plugin-transform-es2015-for-of',
+    'babel-plugin-transform-es2015-sticky-regex',
+    'babel-plugin-transform-es2015-unicode-regex',
+    'babel-plugin-check-es2015-constants',
+    'babel-plugin-transform-es2015-spread',
+    'babel-plugin-transform-es2015-parameters',
+    'babel-plugin-transform-es2015-destructuring',
+    'babel-plugin-transform-es2015-block-scoping',
+    'babel-plugin-transform-es2015-typeof-symbol',
+    ['babel-plugin-transform-regenerator', { 'async': false, 'asyncGenerators': false }]
+];
 
 gulp.task('babel-commonjs', () => {
-    let source = gulp.src('src/*.js');
-
     return gulp.src('src/*.js')
         .pipe(babel({
-            "plugins": ES2015BasePlugins.concat(["babel-plugin-transform-es2015-modules-commonjs"])
+            'plugins': ES2015BasePlugins.concat(['babel-plugin-transform-es2015-modules-commonjs'])
         }))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('babel-amd', () => {
-    let source = gulp.src('src/*.js');
-
     return gulp.src('src/*.js')
         .pipe(babel({
-            "plugins": ES2015BasePlugins.concat(["babel-plugin-transform-es2015-modules-amd"])
+            'plugins': ES2015BasePlugins.concat(['babel-plugin-transform-es2015-modules-amd'])
         }))
         .pipe(rename({suffix: '.amd'}))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('demo', ['babel-amd', 'babel-commonjs'], () => {
+gulp.task('babel-global', () => {
+    return gulp.src('src/*.js')
+        .pipe(babel({
+            'plugins': ES2015BasePlugins.concat([['babel-plugin-globals', { 'globalName': 'window' }]])
+        }))
+        .pipe(rename({suffix: '.global'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('demo', ['babel-amd', 'babel-commonjs', 'babel-global'], () => {
+    gulp.src('dist/*.global.js')
+        .pipe(gulp.dest('demo/vanillajs/js/lib'));
+
     return gulp.src('dist/*.amd.js')
-        .pipe(gulp.dest('demo/js/lib'));
+        .pipe(gulp.dest('demo/amd/js/lib'));
 });
 
 gulp.task('default', ['demo']);
